a6deebcfc665193e221549923c8204a11c59d8d8
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-08-30 19:30:23 +0900

    [Add] 申込者の管理ページで検索機能追加
    受付時の確認を楽にする

---
+++ chrome-extension/search-participant.js
@@ -0,0 +1,34 @@
+"use strict";
+/*
+申込者の管理ページで検索機能（受付時の確認を楽にする）
+*/
+
+{
+	const participants = Array.from(document.querySelectorAll(".ParticipantView")).map(elem => {
+		const number = getTextIfExist(elem, ".number");
+		const displayName = elem.getAttribute("data-username").toLocaleLowerCase();
+		const userName = getTextIfExist(elem, ".user_name").toLocaleLowerCase();
+		return {elem, number, displayName, userName};
+	});
+	const entryTable = document.querySelector(".entirety_area ");
+	const container = document.createElement("div");
+	const input = document.createElement("input");
+	input.style.width = "80%";
+	input.style.display = "block";
+	input.style.margin = "auto";
+	input.placeholder = "受付番号・参加者名・IDで検索（大文字小文字無視）";
+	input.addEventListener("keyup", () => {
+		const value = input.value.toLocaleLowerCase();
+		participants.forEach(({elem, number, displayName, userName}) => {
+			const isMatch = number.startsWith(value) || displayName.includes(value) || userName.includes(value);
+			elem.style.display = isMatch ? "" : "none";
+		});
+	});
+	container.appendChild(input);
+	entryTable.parentNode.insertBefore(container, entryTable.nextElementSibling);
+
+	function getTextIfExist(elem, selector) {
+		const target = elem.querySelector(selector);
+		return target ? target.innerText : "";
+	}
+}
